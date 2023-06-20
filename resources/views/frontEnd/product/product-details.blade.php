5f08e0de7a74a94af4c6a040287ffa231b18df5a
Saniatul Haque Taposh <taposhmd2@gmail.com>
2023-06-20 18:27:19 +0600

    product title change for client

--- resources/views/frontEnd/product/product-details.blade.php
+++ resources/views/frontEnd/product/product-details.blade.php
@@ -21,14 +21,14 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="single-sidebar">
-                        <h2 class="sidebar-title">Search Products</h2>
+                        <h2 class="sidebar-title">Product Search</h2>
                         <form action="#">
                             <input type="text" placeholder="Search products...">
                             <input type="submit" value="Search">
                         </form>
                     </div>
                     <div class="single-sidebar">
-                        <h2 class="sidebar-title">Products</h2>
+                        <h2 class="sidebar-title">Product</h2>
                         <div class="thubmnail-recent">
                             <img src="{{asset('$product->image')}}" class="recent-thumb" alt="">
                             <h2><a href="#">{{$product->product_name}}</a></h2>
